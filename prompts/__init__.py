810d6c767ab7584642731f6f74b54a728399287a
niklas <niklas.finken@aleph-alpha.de>
2023-07-25 14:51:28 +0200

    working MVP

---
+++ prompts/__init__.py
@@ -0,0 +1,4 @@
+from .prompts import (
+    PromptConfig,
+    chain_of_thought_qa_config
+)
---
+++ prompts/definitions/chain_of_thought_qa.py
@@ -0,0 +1,18 @@
+"""
+Prompt to run chain of thought qa (English). Replace the text and question and play around!
+"""
+prompt = """### Instruction:
+For the text below, answer the question (Question). Think step by step to arrive at a solution. If the question cannot be answered, return "Unanswerable".
+
+### Input:
+Text: HR 7484 is a detached eclipsing binary, which means the orbital plane is aligned close to the line-of-sight from the Earth, causing the components to eclipse twice per orbit. The system is undergoing apsidal motion, with a rate greater than that predicted by general relativity. Both components are ordinary F-type main-sequence stars with similar physical properties. The star normally has an apparent magnitude of 5.89, but every 7.64 days (7 days, 15 hours, and 22 minutes) its brightness decreases to magnitude 6.37, approximately two thirds as bright.  Five days and 17 hours after each primary eclipse, there is a secondary eclipse when the brightness drops to magnitude 6.06, about 85% of the normal brightness. Each eclipse lasts for 220 minutes.  Both eclipses are partial.
+
+Question: What type of star is HR 7484?
+
+### Response:"""
+
+settings = {
+    "maximum_tokens": 128
+}
+
+model = "luminous-supreme-control"
